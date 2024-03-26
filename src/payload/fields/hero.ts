374be262205ebc66dd9f3b7d912ee58d1c5a9f4f
Sean Currlin <sean.currlin@gmail.com>
2024-03-26 14:29:17 -0400

    fix TS errors 4

--- src/payload/fields/hero.ts
+++ src/payload/fields/hero.ts
@@ -57,7 +57,7 @@ export const hero: Field = {
       required: true,
       admin: {
         condition: (_, { type } = {}) => 
-      ['highImpact', 'mediumImpact', 'customHero'].includes(type),
+          ['highImpact', 'mediumImpact', 'customHero'].includes(type),
       },
     },
   ],
